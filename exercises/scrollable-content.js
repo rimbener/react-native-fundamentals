7a24cc31f8902981ec47d39e42e9bcf1c8cc757f
hernan.laura.qp <hernan.laura@quickpivot.com>
2020-12-22 08:54:40 -0300

    Update scrollable-content.js

--- exercises/scrollable-content.js
+++ exercises/scrollable-content.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
-  container: { backgroundColor: "#fff" },
+  container: {
+    backgroundColor: "#fff",
+    height: 0
+  },
   box: {
     width: 100,
     height: 100,
@@ -18,4 +22,12 @@ const Square = ({ text, bgColor = "#7ce0f9" }) => (
   </View>
 );
 
-export default () => null;
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+export default () => (
+  <ScrollView style={styles.container} contentContainerStyle={{ backgroundColor: 'red' }}>
+    {data.map((item, index) =>
+      <Square key={index} text={`Square ${item}`}></Square>
+    )}
+  </ScrollView>
+);
