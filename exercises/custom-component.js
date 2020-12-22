cb9d1f92500730d7fd52303f3eaaff0f10ead13c
hernan.laura.qp <hernan.laura@quickpivot.com>
2020-12-22 08:53:21 -0300

    Update custom-component.js

--- exercises/custom-component.js
+++ exercises/custom-component.js
@@ -1,6 +1,32 @@
 // Creating a Custom Component
+import React from 'react';
+import { Text } from "react-native";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const Button = ({ onPress, text, buttonStyle }) => (
+  <TouchableOpacity onPress={onPress}
+    style={{
+      backgroundColor: '#ff637c',
+      alignSelf: 'center',
+      marginVertical: 15,
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      ...buttonStyle
+    }}>
+    <Text style={{ color: '#fff' }}>{text}</Text>
+  </TouchableOpacity>
+)
+
 
 export default () => {
-  // return <Button text="Hello" onPress={() => alert('hello!')} />
-  return null;
+  // Replace return null with your code.
+  return (
+    <>
+      <Button onPress={() => alert('Hello')} text="Say Hello"></Button>
+      <Button
+        text="Say Goobye"
+        onPress={() => alert('Goodbye')}
+        buttonStyle={{ backgroundColor: '#4dc2c2' }}></Button>
+    </>
+  );
 };
